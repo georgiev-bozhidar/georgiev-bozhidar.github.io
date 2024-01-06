6dc856b5e3da264c1b8b90324d52b9fd30097af2
Bozhidar Georgiev <georgiev-bozhidar+github@proton.me>
2024-01-07 00:03:24 +0200

    Transferred files from local folder to repository

---
+++ src/App.tsx
@@ -0,0 +1,150 @@
+/* import { useState } from "react"; */
+/* import reactLogo from "./assets/react.svg";
+ * import viteLogo from "/vite.svg"; */
+import "./App.css";
+import { Code, Art, Music } from "./components/Projects";
+
+function App() {
+  return (
+    <>
+      <div className="site">
+        <div className="hero">
+          <h2 id="hero-name">Bozhidar Georgiev</h2>
+          <div className="hero-nav">
+            <a className="nav" href="">
+              About Me
+            </a>
+            <a className="nav" href="">
+              Portfolio
+            </a>
+            <a className="nav" href="">
+              Something
+            </a>
+            <a className="nav" href="">
+              Contact
+            </a>
+          </div>
+        </div>
+        <div className="main">
+          <div className="about-me">
+            <h3>About Me</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+              at dictum augue, nec tempus sem. Vestibulum elit tellus, faucibus
+              tincidunt dapibus nec, bibendum non justo. Praesent et facilisis
+              mauris. Pellentesque congue nec urna a molestie. Proin tempor leo
+              at eros imperdiet, ac scelerisque risus finibus. Nulla laoreet in
+              eros volutpat ultricies. Donec interdum in augue a scelerisque.
+              Vivamus vel lorem feugiat, ultricies purus nec, aliquet lorem.
+              Nullam accumsan arcu elit, sit amet fermentum velit condimentum
+              elementum. Nunc tempor neque et pulvinar tincidunt. Nulla
+              facilisi. Proin pulvinar orci eget enim gravida aliquam.
+            </p>
+            <p>
+              Aenean quis libero augue. Suspendisse consequat consequat varius.
+              Donec sit amet orci mattis, euismod tellus malesuada, lacinia
+              felis. Ut placerat, urna non cursus pulvinar, nulla neque
+              elementum sapien, a consectetur velit nisi a odio. Nullam lacus
+              leo, sodales aliquet nibh non, placerat laoreet turpis. Curabitur
+              semper sollicitudin magna at aliquet. Vestibulum feugiat turpis id
+              libero pellentesque euismod. Pellentesque in ante suscipit dolor
+              bibendum rutrum.
+            </p>
+            <p>
+              Etiam feugiat non mauris eu egestas. Etiam luctus eget tellus et
+              congue. Proin commodo mi at turpis aliquam porta. Aliquam auctor
+              enim ac ipsum imperdiet consectetur. Integer pharetra sagittis
+              bibendum. In ornare mi metus, eu placerat urna vehicula et.
+              Maecenas non dictum metus. Vivamus malesuada dapibus urna nec
+              fermentum. Etiam efficitur egestas urna, et sollicitudin arcu
+              blandit rhoncus. Sed enim ipsum, lacinia at suscipit ut, facilisis
+              at ante. Nullam quis convallis elit.
+            </p>
+          </div>
+          <div className="portfolio">
+            <div className="container">
+              <h3>Projects</h3>
+              <div className="projects">
+                <Code
+                  imagePath="img/andrea-de-santis-g6nPslCoXDs-unsplash.jpg"
+                  projectName="Project Name"
+                  projectDescription="A simple description of the project that just so happens to be exactly three lines."
+                />
+                <Code
+                  imagePath="img/neom-g6Me5mUQQIQ-unsplash.jpg"
+                  projectName="Project Name"
+                  projectDescription="A simple description of the project that just so happens to be exactly three lines."
+                />
+              </div>
+            </div>
+
+            <div className="container">
+              <h3>Art</h3>
+              <Art
+                imagePath="img/neom-s0A8sa9oasY-unsplash.jpg"
+                artName="Art Name"
+              />
+              <Art
+                imagePath="img/neom-tuEtpjghVmg-unsplash.jpg"
+                artName="Art Name"
+              />
+            </div>
+
+            <div className="container">
+              <h3>Music</h3>
+              <div className="musics">
+                <Music
+                  imagePath="img/ultrakill-infinite-hyperdeath-cover.jpg"
+                  musicPath="audio/Heaven Pierce Her - Into the Fire.mp3"
+                  artistName="Heaven Pierce Her"
+                  musicName="Into the Fire"
+                />
+                <Music
+                  imagePath="img/ultrakill-imperfect-hatred-cover.png"
+                  musicPath="audio/Heaven Pierce Her - Dune Eternal.mp3"
+                  artistName="Heaven Pierce Her"
+                  musicName="Dune Eternal"
+                />
+                <Music
+                  imagePath="img/ultrakill-death-odyssey-cover.jpg"
+                  musicPath="audio/Heaven Pierce Her - Death Odyssey.mp3"
+                  artistName="Heaven Pierce Her"
+                  musicName="Death Odyssey"
+                />
+                <Music
+                  imagePath="img/ultrakill-flesh-prison-cover.png"
+                  musicPath="audio/Heaven Pierce Her - ORDER.mp3"
+                  artistName="Heaven Pierce Her"
+                  musicName="ORDER"
+                />
+                <Music
+                  imagePath="img/ultrakill-pandemonium-cover.jpg"
+                  musicPath="audio/Heaven Pierce Her - WAR.mp3"
+                  artistName="Heaven Pierce Her"
+                  musicName="WAR"
+                />
+                <Music
+                  imagePath="img/ultrakill-1000thr-cover.jpg"
+                  musicPath="audio/Heaven Pierce Her - War Without Reason.mp3"
+                  artistName="Heaven Pierce Her"
+                  musicName="War Without Reason"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+/*
+img/andrea-de-santis-g6nPslCoXDs-unsplash.jpg
+img/neom-g6Me5mUQQIQ-unsplash.jpg
+img/neom-s0A8sa9oasY-unsplash.jpg
+img/neom-tuEtpjghVmg-unsplash.jpg
+img/samuel-arkwright-_V4dS5_NFfs-unsplash.jpg
+img/trevor-vannoy-GtWsGhRdrss-unsplash.jpg
+ */
---
+++ src/components/Projects.tsx
@@ -0,0 +1,77 @@
+interface CodeProps {
+  imagePath: string;
+  imageAlt?: string;
+  projectName?: string;
+  projectDescription?: string;
+}
+
+export function Code({
+  imagePath,
+  imageAlt = "",
+  projectName = "Project Name",
+  projectDescription = "A short description of the project.",
+}: CodeProps) {
+  return (
+    <>
+      <div className="project code">
+        <img src={imagePath} alt={imageAlt} />
+        <div className="project-text">
+          <h4>{projectName}</h4>
+          <p>{projectDescription}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+interface ArtProps {
+  imagePath: string;
+  imageAlt?: string;
+  artName?: string;
+}
+
+export function Art({
+  imagePath,
+  imageAlt = "",
+  artName = "Art Name",
+}: ArtProps) {
+  return (
+    <div className="project art">
+      <img src={imagePath} alt={imageAlt} />
+      <h4>{artName}</h4>
+    </div>
+  );
+}
+
+interface MusicProps {
+  imagePath?: string;
+  imageAlt?: string;
+  musicPath: string;
+  artistName: string;
+  musicName: string;
+}
+
+export function Music({
+  imagePath = "",
+  imageAlt = "",
+  musicPath,
+  artistName,
+  musicName,
+}: MusicProps) {
+  return (
+    <>
+      <div className="project music">
+        <div className="music-image">
+          <img src={imagePath} alt={imageAlt} />
+        </div>
+        <div className="music-main">
+          <div className="music-text">
+            <h4>{artistName}</h4>
+            <p>{musicName}</p>
+          </div>
+          <audio controls src={musicPath}></audio>
+        </div>
+      </div>
+    </>
+  );
+}
