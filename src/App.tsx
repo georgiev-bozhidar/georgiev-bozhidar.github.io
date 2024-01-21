5edfcd6e8378ba36c56ade0289d7716a5f5232bb
Bozhidar Georgiev <georgiev-bozhidar+github@proton.me>
2024-01-21 23:38:41 +0200

    Removed placeholder content

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@
 import "./App.css";
 import { Code, Art, Music } from "./components/Projects";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faReact,
+  faJava,
+  faLinux,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
@@ -18,12 +27,33 @@ function App() {
             <a href="#portfolio" className="nav">
               Portfolio
             </a>
-            <a href="#something" className="nav">
+            <a href="#skills" className="nav">
               Something
             </a>
             <a href="#contact" className="nav">
               Contact
             </a>
+            <div className="socials">
+              <Link to="https://github.com/georgiev-bozhidar" target="_">
+                <FontAwesomeIcon
+                  className="icon"
+                  icon={faGithub}
+                  size={"2x"}
+                  color={"#000000"}
+                />
+              </Link>
+              <Link
+                to="https://linkedin.com/in/bozhidar-georgiev-71b75520a/"
+                target="_"
+              >
+                <FontAwesomeIcon
+                  className="icon"
+                  icon={faLinkedin}
+                  size={"2x"}
+                  color={"#000000"}
+                />
+              </Link>
+            </div>
             {/* <Link to="#about-me" className="nav">
                 About Me
                 </Link>
@@ -41,108 +71,126 @@ function App() {
         <div className="main">
           <div id="about-me">
             <h3>About Me</h3>
+            {/* <h4>Hello!</h4> */}
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              at dictum augue, nec tempus sem. Vestibulum elit tellus, faucibus
-              tincidunt dapibus nec, bibendum non justo. Praesent et facilisis
-              mauris. Pellentesque congue nec urna a molestie. Proin tempor leo
-              at eros imperdiet, ac scelerisque risus finibus. Nulla laoreet in
-              eros volutpat ultricies. Donec interdum in augue a scelerisque.
-              Vivamus vel lorem feugiat, ultricies purus nec, aliquet lorem.
-              Nullam accumsan arcu elit, sit amet fermentum velit condimentum
-              elementum. Nunc tempor neque et pulvinar tincidunt. Nulla
-              facilisi. Proin pulvinar orci eget enim gravida aliquam.
-            </p>
-            <p>
-              Aenean quis libero augue. Suspendisse consequat consequat varius.
-              Donec sit amet orci mattis, euismod tellus malesuada, lacinia
-              felis. Ut placerat, urna non cursus pulvinar, nulla neque
-              elementum sapien, a consectetur velit nisi a odio. Nullam lacus
-              leo, sodales aliquet nibh non, placerat laoreet turpis. Curabitur
-              semper sollicitudin magna at aliquet. Vestibulum feugiat turpis id
-              libero pellentesque euismod. Pellentesque in ante suscipit dolor
-              bibendum rutrum.
-            </p>
-            <p>
-              Etiam feugiat non mauris eu egestas. Etiam luctus eget tellus et
-              congue. Proin commodo mi at turpis aliquam porta. Aliquam auctor
-              enim ac ipsum imperdiet consectetur. Integer pharetra sagittis
-              bibendum. In ornare mi metus, eu placerat urna vehicula et.
-              Maecenas non dictum metus. Vivamus malesuada dapibus urna nec
-              fermentum. Etiam efficitur egestas urna, et sollicitudin arcu
-              blandit rhoncus. Sed enim ipsum, lacinia at suscipit ut, facilisis
-              at ante. Nullam quis convallis elit.
+              Hello! My name is Bozhidar Georgiev. I am a Full Stack Web
+              Developer with <span className="strong">React</span> and{" "}
+              <span className="strong">Spring Boot</span>.
             </p>
           </div>
           <div id="portfolio">
             <h3>Projects</h3>
             <div className="projects">
-              <Code
-                imagePath="/img/andrea-de-santis-g6nPslCoXDs-unsplash.jpg"
-                projectName="Project Name"
-                projectDescription="A simple description of the project that just so happens to be exactly three lines."
-              />
-              <Code
-                imagePath="/img/neom-g6Me5mUQQIQ-unsplash.jpg"
-                projectName="Project Name"
-                projectDescription="A simple description of the project that just so happens to be exactly three lines."
-              />
+              {/* <Code
+                  imagePath="/img/andrea-de-santis-g6nPslCoXDs-unsplash.jpg"
+                  projectName="Project Name"
+                  projectDescription="A simple description of the project that just so happens to be exactly three lines."
+                  />
+                  <Code
+                  imagePath="/img/neom-g6Me5mUQQIQ-unsplash.jpg"
+                  projectName="Project Name"
+                  projectDescription="A simple description of the project that just so happens to be exactly three lines."
+                  /> */}
+              <p>Coming soon...</p>
             </div>
 
             <h3>Art</h3>
-            <Art
-              imagePath="/img/neom-s0A8sa9oasY-unsplash.jpg"
-              artName="Art Name"
-            />
-            <Art imagePath="/img/neom-tuEtpjghVmg-unsplash.jpg" />
+            <div className="art">
+              {/* <Art
+                  imagePath="/img/neom-s0A8sa9oasY-unsplash.jpg"
+                  artName="Art Name"
+                  />
+                  <Art imagePath="/img/neom-tuEtpjghVmg-unsplash.jpg" /> */}
+
+              <p>Coming soon...</p>
+            </div>
 
             <h3>Music</h3>
             <div className="musics">
-              <Music
-                imagePath="/img/ultrakill-infinite-hyperdeath-cover.jpg"
-                artistName="Heaven Pierce Her"
-                musicName="Into the Fire"
-              />
-              <Music
-                imagePath="/img/ultrakill-imperfect-hatred-cover.png"
-                artistName="Heaven Pierce Her"
-                musicName="Dune Eternal"
-              />
-              <Music
-                imagePath="/img/ultrakill-death-odyssey-cover.jpg"
-                artistName="Heaven Pierce Her"
-                musicName="Death Odyssey"
-              />
-              <Music
-                imagePath="/img/ultrakill-flesh-prison-cover.png"
-                artistName="Heaven Pierce Her"
-                musicName="ORDER"
-              />
-              <Music
-                imagePath="/img/ultrakill-pandemonium-cover.jpg"
-                artistName="Heaven Pierce Her"
-                musicName="WAR"
-              />
-              <Music
-                imagePath="/img/ultrakill-1000thr-cover.jpg"
-                artistName="Heaven Pierce Her"
-                musicName="War Without Reason"
-              />
+              {/* <Music
+                  imagePath="/img/ultrakill-infinite-hyperdeath-cover.jpg"
+                  artistName="Heaven Pierce Her"
+                  musicName="Into the Fire"
+                  />
+                  <Music
+                  imagePath="/img/ultrakill-imperfect-hatred-cover.png"
+                  artistName="Heaven Pierce Her"
+                  musicName="Dune Eternal"
+                  /> */}
+              <p>Coming soon...</p>
             </div>
           </div>
-          <div id="something">
-            <h3 id="something">Something</h3>
+          <div id="skills">
+            <h3>Skills</h3>
+            <ul className="skill-list front-end">
+              <li className="front-end">
+                <ul>
+                  <li>
+                    <h4>
+                      <span className="strong">Front End</span>
+                    </h4>
+                  </li>
+                  <li>
+                    <ul>
+                      <li>
+                        <FontAwesomeIcon icon={faCode} color={"#000000"} />{" "}
+                        HTML/CSS
+                      </li>
+                      <li>
+                        <FontAwesomeIcon icon={faReact} color={"#000000"} />{" "}
+                        React
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li className="back-end">
+                <ul>
+                  <li>
+                    <h4>
+                      <span className="strong">Back End</span>
+                    </h4>
+                  </li>
+                  <li>
+                    <ul>
+                      <li>
+                        <FontAwesomeIcon icon={faJava} color={"#000000"} /> Java
+                      </li>
+                      <li>Spring Boot</li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li className="other">
+                <ul>
+                  <li>
+                    <h4>
+                      <span className="strong">Other</span>
+                    </h4>
+                  </li>
+                  <li>
+                    <ul>
+                      <li>
+                        <FontAwesomeIcon icon={faLinux} color={"#000000"} />{" "}
+                        Linux
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </div>
           <div id="contact">
-            <h3 id="contact">Contact</h3>
+            <h3>Contact</h3>
           </div>
         </div>
         <div className="footer">
           <footer>
             <p>
               <span className="temporary">
-                Everything shown is placeholder material. Images used are from
-                Unsplash.
+                Everything shown is placeholder material.
+                <br />
+                Images used are from Unsplash.
                 <br /> Music and covers used are from{" "}
                 <Link to="http://devilmayquake.com" target="_blank">
                   ULTRAKILL
